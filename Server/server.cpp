2026f624e0e9dca70a6c3cac480a1f50c0e005cd
MMMX Server <mmmxserver@gmail.com>
2021-02-16 18:41:58 +0300

    CMakeLists.txt fixes

--- Server/server.cpp
+++ Server/server.cpp
@@ -1,8 +1,10 @@
 #include <QCoreApplication>
 #include <QDebug>
 
+#include "Controller/abstract_controller.h"
+
 int main(int argc, char* argv[]) {
   QCoreApplication a(argc, argv);
-  qDebug() << "Hello World!";
+  qDebug() << "Server started";
   return QCoreApplication::exec();
 }
