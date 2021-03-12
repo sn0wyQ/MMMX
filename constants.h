1d6f0330b1d89c739f74c04a0ab4637a6cabfac3
sn0wyQ <soezforkm@gmail.com>
2021-03-12 18:47:57 +0300

    Added EventTypeWrapper and #define ONLINE true/false to select if server is localhost or not

--- constants.h
+++ constants.h
@@ -4,6 +4,8 @@
 #include <QString>
 #include <QUrl>
 
+#include "defines.h"
+
 // code_readability++;
 using ClientId = int;
 using GameObjectId = int;
@@ -20,9 +22,13 @@ const QString kVersionString = "0.0.1";
 // Server
 const QString kServerName = "MMMX Server";
 constexpr int kServerPort = 1337;
-// const QString kServerIp = QString("ws://") + "188.120.224.70";
-const QString kServerIp = QString("ws://") + "localhost";
-const QUrl kServerUrl = QUrl(kServerIp + ":" + QString::number(kServerPort));
+#if ONLINE
+const QString kServerIp = "188.120.224.70";
+#else
+const QString kServerIp = "localhost";
+#endif
+const QUrl kServerUrl =
+    QUrl(QString("ws://") + kServerIp + ":" + QString::number(kServerPort));
 constexpr int kTickrate = 64;
 constexpr int kTimeToTick = 1000 / kTickrate;
 
---
+++ defines.h
@@ -0,0 +1,6 @@
+#ifndef DEFINES_H_
+#define DEFINES_H_
+
+#define ONLINE false
+
+#endif  // DEFINES_H_
