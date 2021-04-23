547adb59d053d8040c49104120e53563e925e64d
kmekhovich <mekhovichi@ya.ru>
2021-04-23 16:55:25 +0300

    fixed bug

--- Client/client_controller.cpp
+++ Client/client_controller.cpp
@@ -175,9 +175,12 @@ void ClientController::UpdateLocalBullets(int delta_time) {
     auto object_collided = ObjectCollision::GetObjectBulletCollidedWith(
             bullet, model_->GetAllGameObjects(), delta_time, true);
     if (object_collided != nullptr) {
-      is_need_to_delete.push_back(bullet->GetId());
+      bullet->SetIsNeedToDelete(true);
     }
     bullet->OnTick(delta_time);
+    if (bullet->IsNeedToDelete()) {
+      is_need_to_delete.push_back(bullet->GetId());
+    }
   }
 
   for (const auto& bullet_id : is_need_to_delete) {
