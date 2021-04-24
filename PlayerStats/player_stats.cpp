6e558838620d2b20227c28bb9c6f4ba45e2253cf
OS-M <manalog44@gmail.com>
2021-04-24 20:46:20 +0300

    Fixed params order

--- PlayerStats/player_stats.cpp
+++ PlayerStats/player_stats.cpp
@@ -7,22 +7,22 @@ PlayerStats::PlayerStats(GameObjectId player_id,
                                            nickname_{std::move(nickname)} {}
 
 void PlayerStats::SetParams(std::vector<QVariant> params) {
-  player_id_ = params.back().toInt();
-  params.pop_back();
-  this->SetNickname(params.back().toString());
+  this->SetDeaths(params.back().toInt());
   params.pop_back();
   this->SetKills(params.back().toInt());
   params.pop_back();
-  this->SetDeaths(params.back().toInt());
+  this->SetNickname(params.back().toString());
+  params.pop_back();
+  player_id_ = params.back().toInt();
   params.pop_back();
 }
 
 std::vector<QVariant> PlayerStats::GetParams() const {
   std::vector<QVariant> params;
-  params.emplace_back(this->GetDeaths());
-  params.emplace_back(this->GetKills());
-  params.emplace_back(this->GetNickname());
   params.emplace_back(this->GetPlayerId());
+  params.emplace_back(this->GetNickname());
+  params.emplace_back(this->GetKills());
+  params.emplace_back(this->GetDeaths());
   return params;
 }
 
