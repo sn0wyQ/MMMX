39e62439de052d8d00a4495934f581d47ff9cbf0
OS-M <manalog44@gmail.com>
2021-04-29 22:36:24 +0300

    Working json creep settings

---
+++ GameObject/MovableObject/Entity/Creep/creep_settings.cpp
@@ -0,0 +1,5 @@
+//
+// Created by matvey on 29.04.21.
+//
+
+#include "creep_settings.h"
---
+++ GameObject/MovableObject/Entity/Creep/creep_settings.h
@@ -0,0 +1,8 @@
+//
+// Created by matvey on 29.04.21.
+//
+
+#ifndef CREEP_SETTINGS_H_
+#define CREEP_SETTINGS_H_
+
+#endif  // CREEP_SETTINGS_H_
