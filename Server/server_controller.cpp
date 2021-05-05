2c7a88b6a581e66c82aa75a7687c232e5c99b290
kmekhovich <mekhovichi@ya.ru>
2021-05-05 11:53:10 +0300

    Added remote param

--- Server/server_controller.cpp
+++ Server/server_controller.cpp
@@ -4,8 +4,7 @@ ServerController::ServerController()
   : web_socket_server_(Constants::kServerName,
                        QWebSocketServer::NonSecureMode) {
   if (web_socket_server_.listen(QHostAddress::Any,  Constants::kServerPort)) {
-    qInfo().noquote().nospace() << "Server is running on "
-      << Constants::kServerUrl.host() << ":"
+    qInfo().noquote().nospace() << "Server is running on port "
       << web_socket_server_.serverPort();
     connect(&web_socket_server_,
             &QWebSocketServer::newConnection,
