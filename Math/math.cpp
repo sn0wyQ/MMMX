0552568e4ee0291b8adfdc42ef0d19aee62b74e3
Matvey <manalog44@gmail.com>
2021-05-23 20:06:47 +0300

    Raycasting hotfix (#114)

--- Math/math.cpp
+++ Math/math.cpp
@@ -13,9 +13,6 @@ float Math::RadiansToDegrees(float radians) {
 }
 
 bool Math::IsPointOnSegment(QPointF first, QPointF second, QPointF point) {
-  if (!IsPointOnLine(point, Line(first, second))) {
-    return false;
-  }
   float min_x = std::min(first.x(), second.x());
   float max_x = std::max(first.x(), second.x());
   float min_y = std::min(first.y(), second.y());
@@ -65,33 +62,33 @@ std::vector<QPointF> Math::GetRotatedRect(const QRectF& rect, float rotation) {
   return points;
 }
 
-Math::Line::Line(float a_, float b_, float c_) : a(a_), b(b_), c(c_) {}
+Math::Line::Line(double a_, double b_, double c_) : a(a_), b(b_), c(c_) {}
 
 Math::Line::Line(QPointF point1_, QPointF point2_) : point1(point1_),
                                                      point2(point2_) {
-  float x1 = point1_.x();
-  float y1 = point1_.y();
-  float x2 = point2_.x();
-  float y2 = point2_.y();
+  double x1 = point1_.x();
+  double y1 = point1_.y();
+  double x2 = point2_.x();
+  double y2 = point2_.y();
   a = y2 - y1;
   b = x1 - x2;
   c = x2 * y1 - x1 * y2;
 }
 
 QPointF Math::GetLinesIntersection(const Line& line1, const Line& line2) {
-  float a1 = line1.a;
-  float b1 = line1.b;
-  float c1 = line1.c;
-  float a2 = line2.a;
-  float b2 = line2.b;
-  float c2 = line2.c;
+  double a1 = line1.a;
+  double b1 = line1.b;
+  double c1 = line1.c;
+  double a2 = line2.a;
+  double b2 = line2.b;
+  double c2 = line2.c;
 
   if (std::fabs(b2 * a1 - b1 * a2) < kEps) {
     return {};
   }
 
-  float x = (b1 * c2 - b2 * c1) / (b2 * a1 - b1 * a2);
-  float y;
+  double x = (b1 * c2 - b2 * c1) / (b2 * a1 - b1 * a2);
+  double y;
   if (std::fabs(b1) > kEps) {
     y = -(a1 * x + c1) / b1;
   } else {
@@ -100,17 +97,17 @@ QPointF Math::GetLinesIntersection(const Line& line1, const Line& line2) {
   return {x, y};
 }
 
-float Math::GetDistanceFromPointToLine(const QPointF& point,
+double Math::GetDistanceFromPointToLine(const QPointF& point,
                                        const Line& line) {
-  float a = line.a;
-  float b = line.b;
-  float c = line.c;
+  double a = line.a;
+  double b = line.b;
+  double c = line.c;
   return std::fabs(a * point.x() + b * point.y() + c) /
       std::sqrt(a * a + b * b);
 }
 
 bool Math::IsPointOnCircle(const QPointF& point, const QPointF& circle_center,
-                           float circle_radius) {
+                           double circle_radius) {
   return std::fabs(DistanceBetweenPoints(point, circle_center) - circle_radius)
       < kEps;
 }
@@ -120,41 +117,43 @@ bool Math::IsPointOnLine(const QPointF& point, const Line& line) {
 }
 
 std::vector<QPointF> Math::GetCircleAndLineIntersections(
-    const QPointF& circle_center, float circle_radius, const Line& line) {
-  float center_x = circle_center.x();
-  float center_y = circle_center.y();
+    const QPointF& circle_center, double circle_radius, const Line& line) {
+  double center_x = circle_center.x();
+  double center_y = circle_center.y();
 
-  float perp_a = line.b;
-  float perp_b = -line.a;
-  float perp_c = line.c;
+  double perp_a = line.b;
+  double perp_b = -line.a;
+  double perp_c = line.c;
 
   if (std::fabs(perp_a) > Math::kEps) {
-    float temp_x = (-perp_b * center_y - perp_c) / perp_a;
+    double temp_x = (-perp_b * center_y - perp_c) / perp_a;
     perp_c -= perp_a * (center_x - temp_x);
   } else {
-    float temp_y = (-perp_a * center_x - perp_c) / perp_b;
+    double temp_y = (-perp_a * center_x - perp_c) / perp_b;
     perp_c -= perp_b * (center_y - temp_y);
   }
 
   QPointF tangent_point = GetLinesIntersection(line,
                                                Line(perp_a, perp_b, perp_c));
-  float a = line.a;
-  float b = line.b;
-  float distance_to_line = GetDistanceFromPointToLine(circle_center, line);
-  float segment_length =
+  double a = line.a;
+  double b = line.b;
+  double distance_to_line = GetDistanceFromPointToLine(circle_center, line);
+  double segment_length =
       std::sqrt(std::fabs(circle_radius * circle_radius - distance_to_line *
           distance_to_line));
-  float delta_y = segment_length * sin(atan(-a / b));
-  float delta_x =
+  double delta_y = segment_length * sin(atan(-a / b));
+  double delta_x =
       std::sqrt(std::fabs(segment_length * segment_length - delta_y * delta_y));
   QPointF res1(tangent_point.x() + delta_x, tangent_point.y() + delta_y);
   QPointF res2(tangent_point.x() - delta_x, tangent_point.y() - delta_y);
   std::vector<QPointF> result;
   if (IsPointOnCircle(res1, circle_center, circle_radius) &&
+      IsPointOnLine(res1, line) &&
       IsPointOnSegment(line.point1, line.point2, res1)) {
     result.push_back(res1);
   }
   if (IsPointOnCircle(res2, circle_center, circle_radius) &&
+      IsPointOnLine(res2, line) &&
       IsPointOnSegment(line.point1, line.point2, res2)) {
     result.push_back(res2);
   }
@@ -163,7 +162,7 @@ std::vector<QPointF> Math::GetCircleAndLineIntersections(
 
 std::vector<QPointF> Math::GetRectWithLineIntersections(const QRectF& rect,
                                                         const Line& line,
-                                                        float angle) {
+                                                        double angle) {
   std::vector<QPointF> answer;
   auto rect_points = GetRotatedRect(rect, angle);
   rect_points.push_back(rect_points[0]);
@@ -172,14 +171,16 @@ std::vector<QPointF> Math::GetRectWithLineIntersections(const QRectF& rect,
     auto point = GetLinesIntersection(rect_line, line);
     if (!point.isNull() &&
         IsPointOnSegment(rect_line.point1, rect_line.point2, point) &&
-        IsPointOnSegment(line.point1, line.point2, point)) {
+        IsPointOnLine(point, rect_line) &&
+        IsPointOnSegment(line.point1, line.point2, point) &&
+        IsPointOnLine(point, line)) {
       answer.push_back(point);
     }
   }
   return answer;
 }
 
-QPointF Math::PointToNewCoordinates(const QPointF& point, float angle) {
+QPointF Math::PointToNewCoordinates(const QPointF& point, double angle) {
   return {point.x() * std::cos(angle) + point.y() * std::sin(angle),
           -point.x() * std::sin(angle) + point.y() * std::cos(angle)};
 }
